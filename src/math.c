942bf6e67cf9f7eb1c644265cd97d8d86ee161db
Bruno Macabeus <macabeus@users.noreply.github.com>
2026-03-15 23:00:34 +0000

    Match FUN_080009c2 (#25)
    * match FUN_080009c2
    
    * generate_asm: fix bug when adding `INCLUDE_ASM` for matched functions

--- src/math.c
+++ src/math.c
@@ -8,6 +8,8 @@ s16 FUN_08000960(s16 arg0, s16 arg1) {
 }
 INCLUDE_ASM("asm/nonmatchings/math", FUN_08000978);
 INCLUDE_ASM("asm/nonmatchings/math", FUN_080009a8);
-INCLUDE_ASM("asm/nonmatchings/math", FUN_080009c2);
+s16 FUN_080009c2(s16 arg0) {
+    return FUN_080518a4(0x100, arg0);
+}
 INCLUDE_ASM("asm/nonmatchings/math", FUN_080009da);
 INCLUDE_ASM("asm/nonmatchings/math", FUN_08000ab2);
