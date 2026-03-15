a40a1c0341460a59d2bb4dd1a38b8d5015562de4
macabeus <bruno.macabeus@gmail.com>
2026-03-15 20:57:18 +0000

    match FUN_08000960

--- src/math.c
+++ src/math.c
@@ -1,7 +1,11 @@
 #include "global.h"
 #include "include_asm.h"
 
-INCLUDE_ASM("asm/nonmatchings/math", FUN_08000960);
+s16 FUN_080518a4(s32 a, s16 b);
+
+s16 FUN_08000960(s16 arg0, s16 arg1) {
+    return (s16)FUN_080518a4((s16)arg0 << 8, arg1);
+}
 INCLUDE_ASM("asm/nonmatchings/math", FUN_08000978);
 INCLUDE_ASM("asm/nonmatchings/math", FUN_080009a8);
 INCLUDE_ASM("asm/nonmatchings/math", FUN_080009c2);
