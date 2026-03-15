c7b0c49ff0ffba248a7c0de8d2b17ceb88bdc993
macabeus <bruno.macabeus@gmail.com>
2026-03-15 21:46:15 +0000

    match FUN_080009c2

--- src/math.c
+++ src/math.c
@@ -4,6 +4,10 @@
 INCLUDE_ASM("asm/nonmatchings/math", FUN_08000960);
 INCLUDE_ASM("asm/nonmatchings/math", FUN_08000978);
 INCLUDE_ASM("asm/nonmatchings/math", FUN_080009a8);
-INCLUDE_ASM("asm/nonmatchings/math", FUN_080009c2);
+s16 FUN_080518a4(s16 a, s16 b);
+
+s16 FUN_080009c2(s16 arg0) {
+    return FUN_080518a4(0x100, arg0);
+}
 INCLUDE_ASM("asm/nonmatchings/math", FUN_080009da);
 INCLUDE_ASM("asm/nonmatchings/math", FUN_08000ab2);
