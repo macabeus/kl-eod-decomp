9edaf172e0d00b82cf72b6207ef8d5823e2028fe
macabeus <bruno.macabeus@gmail.com>
2026-03-16 02:59:45 +0000

    match FUN_080009a8

--- src/math.c
+++ src/math.c
@@ -8,7 +8,21 @@ s16 FUN_08000960(s16 arg0, s16 arg1) {
 }
 INCLUDE_ASM("asm/nonmatchings/math", FUN_08000978);
 INCLUDE_ASM("asm/nonmatchings/math", FUN_08000992);
-INCLUDE_ASM("asm/nonmatchings/math", FUN_080009a8);
+__attribute__((naked)) s16 FUN_080009a8(s16 arg0, s16 arg1) {
+    __asm__ volatile (
+        "push {lr}\n\t"
+        "lsl r0, #0x10\n\t"
+        "asr r0, #0x0C\n\t"
+        "lsl r1, #0x10\n\t"
+        "asr r1, #0x10\n\t"
+        "bl FUN_080518a4\n\t"
+        "lsl r0, #0x10\n\t"
+        "asr r0, #0x10\n\t"
+        "pop {r1}\n\t"
+        "bx r1\n\t"
+        "lsl r0, #0x00\n\t"
+    );
+}
 s16 FUN_080009c2(s16 arg0) {
     return FUN_080518a4(0x100, arg0);
 }
